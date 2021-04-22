bb2e7a303e29a63e1978f4ef36ceb2c4078ce110
Marcos Domingues <dfenixweb@gmail.com>
2021-04-22 15:20:58 -0300

    "Commit inicial do projeto"

---
+++ modules/studentModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    require: true,
+    validate(value) {
+      min: 0;
+    },
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const studentModel = mongoose.model("student", studentSchema, "student");
+
+export { studentModel };
---
+++ routes/studentRouter.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { studentModel } from "../modules/studentModel.js";
+
+const app = express();
+
+//CREATE
+app.post("/student", async (req, res) => {
+  try {
+    const student = new studentModel(req.body);
+    await student.save();
+
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//GET
+app.get("/student", async (req, res) => {
+  try {
+    const student = await studentModel.find({});
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//UPDATE
+app.patch("/student/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await studentModel.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//DELETETE
+app.delete("/student/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await studentModel.findByIdAndDelete({ _id: id }, req.body);
+    if (!data) {
+      res.status(404).send("Documento não encontrado");
+    } else {
+      res.status(200).send();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export { app as studentRouter };
