215d6731df252811e9d10e298d78d1a3466bff60
RacconWebFenix <dfenixweb@gmail.com>
2021-04-23 08:48:58 -0300

    Adicionado PUT

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -52,5 +52,18 @@ app.delete("/student/:id", async (req, res) => {
   }
 });
 
+//PUT
+app.patch("/student/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await studentModel.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 
 export { app as studentRouter };
