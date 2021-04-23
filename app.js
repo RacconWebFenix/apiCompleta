5064ee987806e0f1c4afb946eb3648acb25e6049
Marcos Domingues <dfenixweb@gmail.com>
2021-04-23 07:17:01 -0300

    Ecapsulando variaveis de ambiente com dotenv

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import { studentRouter } from "./routes/studentRouter.js";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+process.env.USERDB;
+process.env.PWDDB;
 
 //conexxão com do mongoose com momgo db
 const con = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://raccon:Webfenix2212!@bootcampigti.gdpcx.mongodb.net/grades?retryWrites=true&w=majority",
+      "mongodb+srv://"+ process.env.USERDB +':'+ process.env.PWDDB + "@bootcampigti.gdpcx.mongodb.net/grades?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -26,4 +32,4 @@ const app = express();
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000, () => console.log("API START"));
+app.listen(process.env.PORT, () => console.log("API START"));
